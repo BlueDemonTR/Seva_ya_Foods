4614c4fe6a04e6c941fdb342b3faace6f5231a43
BlueDemonTR <83763146+BlueDemonTR@users.noreply.github.com>
2025-02-09 13:20:41 +0300

    Initial commit

---
+++ src/client/java/poyoraz/seva_ya_foods/SevaYaFoodsClient.java
@@ -0,0 +1,10 @@
+package poyoraz.seva_ya_foods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SevaYaFoodsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
